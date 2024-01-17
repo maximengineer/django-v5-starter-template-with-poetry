e0dbd898a398758c38df6ace6183a04495c34d2a
allhailpython <allhailpython@protonmail.com>
2024-01-17 08:49:53 +0000

    created .pre-commit-config.yaml, added rules, updated flake8 rules

--- core/backend/settings/base.py
+++ core/backend/settings/base.py
@@ -6,7 +6,6 @@ SECRET_KEY = NotImplemented
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,7 +47,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.backend.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -58,7 +56,6 @@ DATABASES = {
         "NAME": "D:\\projects\\django_template\\db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -78,7 +75,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -89,7 +85,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
